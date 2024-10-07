6fd51e1b19f3c9bb00ff9ed231311a58ecef51a4
Vladislav Dobriyan <127337968+Desstori@users.noreply.github.com>
2024-10-07 12:57:00 +0200

    Added code

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println(adder.add(1, 2));
         Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(6, 3));
-
+//////
     ///
 
     }
